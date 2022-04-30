0196417d05ac99eb499bd90d4f4b8ba95d113539
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-04-29 17:40:12 -0700

    Created loop for seat selection
    Added functionality for selecting seats.

--- src/reservationSystemCPP/reservationSystemCPP/main.cpp
+++ src/reservationSystemCPP/reservationSystemCPP/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 // Function declorations (prototypes)
 char **CreateArrayOfSeats (int numberOfRows, int numberOfSeats);
 void InitializeSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSeats);
-void DisplayArrayOfSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSeats);
+void DisplayArrayOfSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSeats, int seatsAvailable, int seatCount);
 void MemoryCleanup (char **ArrayOfSeats, int numberOfRows, int numberOfSeats);
 // Added my input validator
 int inputValidator (const char *prompt);
@@ -29,22 +29,58 @@ int main(int argc, const char * argv[]) {
     char seatSelection;   // 'A' to max seats, input from user, convert to num
     int row;              // Index into ArrayOfSeats, 0 to numberOfRows -1
     int seat;             // Index into ArrayOfSeats, 0 to numbersOfSeats -1
+    int seatCount = 0;    // Counter for seats taken
+    int seatsAvailable;   // Counter for seats available
+    
     
     // Get the number of numberOfRows and seats from the user
-    // Might use inputValidator() ======================
+    // User input checked for valid numbers to create an array using
+    // inputValidator()
     numberOfRows = inputValidator("Enter the number for numberOfRows: ");
     numberOfSeats = inputValidator("Enter the number for numberOfSeats: ");
-    /*
-    cout << "Enter the number for numberOfRows: ";
-    cin  >> numberOfRows;
-    cout << "Enter the number for numberOfSeats: " ;
-    cin  >> numberOfSeats;*/
-    //==================================================
-    
+    seatsAvailable = numberOfRows * numberOfSeats;
     // Set up array
     ArrayOfSeats = CreateArrayOfSeats(numberOfRows, numberOfSeats);
     InitializeSeats(ArrayOfSeats, numberOfRows, numberOfSeats);
-    DisplayArrayOfSeats(ArrayOfSeats, numberOfRows, numberOfSeats);
+    DisplayArrayOfSeats(ArrayOfSeats, numberOfRows, numberOfSeats, seatsAvailable, seatCount);
+    
+    // Get seat selections
+    do {
+        cout << endl << "Enter a seat selection" << endl;
+        cout << "  (example 5F -or- 00 to Quit: ";
+        cin  >> rowSelection;    // Get row from user
+        cin  >> seatSelection;   // Get seat from user
+        
+        // Did user quit?
+        if (rowSelection == '0')
+            continue;              // Skip the rest of the loop
+        
+        // Convert to uppercase
+        seatSelection = toupper(seatSelection);
+        
+        // Count from zero to work with array
+        row = rowSelection - '1';
+        // Convert A to 0, B to 1 etc
+        seat = seatSelection - 'A';
+        
+        // Verify seat is available here=======
+        if (ArrayOfSeats[row][seat] == '-')
+            cout << "That seat has already been selected" << endl;
+        else {
+            ArrayOfSeats[row][seat] = '-';
+            seatsAvailable--;
+            seatCount++;
+            
+        }   // End of else
+        //=====================================
+        
+        // Display seats
+        DisplayArrayOfSeats(ArrayOfSeats, numberOfRows, numberOfSeats, seatsAvailable, seatCount);
+        
+    }   while (rowSelection != '0');   // End of do/while
+    
+    // Cleanup
+    MemoryCleanup(ArrayOfSeats, numberOfRows, numberOfSeats);
     
     return 0;
 }   // End of main
@@ -77,7 +113,7 @@ void InitializeSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSeats) 
 }   // End of InitializeSeats
 
 // Start of DisplayArrayOfSeats
-void DisplayArrayOfSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSeats) {
+void DisplayArrayOfSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSeats, int seatsAvailable, int seatCount) {
     // For each row
     for (int r = 0; r < numberOfRows; r++) {
         cout.width(2);
@@ -88,7 +124,8 @@ void DisplayArrayOfSeats (char **ArrayOfSeats, int numberOfRows, int numberOfSea
         // New line each row
         cout << endl;
     }   // End of loop
-    
+    cout << "Seats Available: " << seatsAvailable << endl;
+    cout << "Seats Taken    : " << seatCount << endl;
 }   // End of DisplayArrayOfSeats
 
 // Start MemoryCleanup
